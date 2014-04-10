cc344b537612b0aadd8aa253aeefbf6d91607514
jimihaze <jimihaze23@gmail.com>
2014-04-10 17:42:07 -0400

    node list graph, split first input into array, set local name with second line input, for loops for creating nodes and adding them to the graph starting with local

--- DistanceVector.java
+++ DistanceVector.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
@@ -6,11 +7,23 @@ import java.util.List;
 
 public class DistanceVector {
 
-	static node local;
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static String local;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static List<node> graph = new ArrayList<node>();
 	
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws IOException {
+		String[] initNode = br.readLine().split("//s+");
+		local = br.readLine();
+		for(int i=0; i<initNode.length; i++){
+			if(initNode[i] == local){
+				graph.add(new node(local));
+			}
+		}
+		for(int i=0; i<initNode.length; i++){
+			if(initNode[i] != local){
+				graph.add(new node(initNode[i]));
+			}
+		}
 	}
 
 }
@@ -18,6 +31,10 @@ public class DistanceVector {
 class node {
 	String name;
 	List<edge> edges = new ArrayList<edge>();
+	
+	public node(String n){
+		name = n;
+	}
 }
 
 class edge {
