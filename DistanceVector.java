c750d70748dc12b2cfd325596fd9eb4f84898027
jimihaze <jimihaze23@gmail.com>
2014-04-10 17:13:55 -0400

    added buffered reader

--- DistanceVector.java
+++ DistanceVector.java
@@ -1,7 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class DistanceVector {
 
 	static node local;
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
 	public static void main(String[] args) {
 
@@ -9,12 +15,12 @@ public class DistanceVector {
 
 }
 
-public class node {
+class node {
 	String name;
 	List<edge> edges = new ArrayList<edge>();
 }
 
-public class edge {
+class edge {
 	String target;
 	int cost;
 }
