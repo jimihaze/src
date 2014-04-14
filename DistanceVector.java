ef0d52b0be87148a21a81ed94344402025449530
jimihaze <jimihaze23@gmail.com>
2014-04-13 23:09:22 -0400

    Realized im an idiot, changed everything. No fauxlocal, its unecessary, added distToLocal and Adj || for initialize process. Removed additional input logic.

--- DistanceVector.java
+++ DistanceVector.java
@@ -11,7 +11,6 @@ public class DistanceVector {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static List<node> graph = new ArrayList<node>();
 	static node localNode = null;
-	static node fauxLocal = null;
 	static int DVlength;
 
 	public static void main(String[] args) throws IOException {
@@ -19,39 +18,28 @@ public class DistanceVector {
 		DVlength = initNode.length;
 		local = br.readLine();  //Second input, local node
 
-		localNode = new node(local, DVlength);
+		localNode = new node(local);
 		graph.add(localNode);
 
 		for(int i=0; i<initNode.length; i++){  //Adds all non local nodes to the graph
 			if(initNode[i] != local){
-				graph.add(new node(initNode[i], DVlength));
+				graph.add(new node(initNode[i]));
 			}
 		}
 
-		fauxLocal = localNode;  //Faux local node to add edges between nodes, starts at local
 		String input;
-		while((input = br.readLine()) != null){  //Loop on read line, to take in more edges
-			if(input.isEmpty()){
-				continue;
-			}
+		while((input = br.readLine()) != null){
 			String[] edgeParams = input.split("//s+");
-			if(edgeParams.length == 1){  //Determines new faux local to add edges to that node
-				for(node n: graph){
-					if(n.name == edgeParams[0]){
-						fauxLocal = n;
-					}
+			node currentNode = null;
+			for(node n: graph){
+				if(n.name == edgeParams[0]){
+					currentNode = n;
+					currentNode.setAdjacent(true);
+					currentNode.setDist(Integer.parseInt(edgeParams[1]));
 				}
 			}
-			else{  //Actually adds edges between nodes given costs
-				node currentNode = null;
-				for(node n: graph){
-					if(n.name == edgeParams[0]){
-						currentNode = n;
-					}
-				}
-				fauxLocal.edges.add(new edge(currentNode, Integer.parseInt(edgeParams[1])));
-				currentNode.edges.add(new edge(localNode, Integer.parseInt(edgeParams[1])));
-			}
+			localNode.edges.add(new edge(currentNode, Integer.parseInt(edgeParams[1])));
+			currentNode.edges.add(new edge(localNode, Integer.parseInt(edgeParams[1])));
 		}
 	}
 }
@@ -59,18 +47,26 @@ public class DistanceVector {
 class node {
 	public String name;
 	public List<edge> edges = new ArrayList<edge>();
-	public int[] DV;
+	public boolean adjToLocal;
+	public int distToLocal;
 	
-	public node(String n, int l){
+	public node(String n){
 		name = n;
-		DV = new int[l];
 	}
-	
+
 	public void updateDV(){
 		/**Bellman Ford
 		For all nodes z in the graph that are not the local node
 		The distance from local to z is the min(costofedge(x,y) + Dy(z))
-		**/
+		 **/
+	}
+
+	public void setAdjacent(boolean b){
+		adjToLocal = b;
+	}
+	
+	public void setDist(int i){
+		distToLocal = i;
 	}
 }
 
